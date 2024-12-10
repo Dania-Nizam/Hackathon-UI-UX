6dbfccbb15cbb15bc14ef3c7070b842988f8de64
Dania-Nizam <almirashah35@gmail.com>
2024-12-09 23:41:10 -0800

    change

--- src/app/components/Detail.tsx
+++ src/app/components/Detail.tsx
@@ -50,25 +50,8 @@ const Detail = () => {
                 Nissan GT - R
               </h1>
               <div className="flex mb-4 items-center">
-                {[...Array(4)].map((_, idx) => (
-                  <svg
-                    key={idx}
-                    fill="currentColor"
-                    className="w-4 h-4 text-[#FBAD39]"
-                    viewBox="0 0 24 24"
-                    aria-label="Star rating"
-                  >
-                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-                  </svg>
-                ))}
-                <svg
-                  fill="none"
-                  className="w-4 h-4 text-[#FBAD39]"
-                  viewBox="0 0 24 24"
-                  aria-label="Star rating"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-                </svg>
+                
+                
                 <span className="text-gray-600 ml-3">440+ Reviewer</span>
               </div>
               <p className="leading-relaxed mb-6">
